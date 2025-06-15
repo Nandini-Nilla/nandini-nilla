136c9087c5b0a0dad8b0732d8329a8f796e36f92
nandini <nandininilla@gmail.com>
2025-06-15 14:14:35 +0530

    Updated About section with new image and layout fixes

--- src/components/Sections/About.tsx
+++ src/components/Sections/About.tsx
@@ -15,7 +15,7 @@ const About = () => {
             {" "}
             {/* Add bottom margin for mobile, adjust as needed (mb-4 is 1rem) */}
             <img
-              src="src/assets/myimage.jpg"
+              src="/myimage.jpg"
               alt="Nandini"
               className="rounded-lg shadow-xl w-[300px] h-[300px] object-cover"
             />
